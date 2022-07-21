16d39f0b4662c1038491f52a79e33bdba62253ce
ahyas <ahyasw@gmail.com>
2022-07-21 15:38:20 +0900

    Repair modal

--- resources/views/crud/notification/index.blade.php
+++ resources/views/crud/notification/index.blade.php
@@ -5,7 +5,15 @@
                 <p id="alertHeading">Notification</p><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
-                <p><img src="{{asset('public/logo/checked.png')}}" style="float:left; padding-right:10px;"/><span id="alertMsg"></span></p>
+                <div class='container2'>
+                    <div>
+                        <img src="{{asset('public/logo/checked.png')}}" class='iconDetails'/>
+                    </div>
+                    <div style="margin-left:50px;"> 
+                        <span id="alertMsg"></span>
+                    </div>
+                </div>
+                <br>
                 <div class="modal-footer">
                     <button class="btn btn-sm" data-dismiss="modal" style="margin-left:auto; margin-right:auto;">OK</button>
                 </div>
